a53d7734ae8626ac8acb53efabc844f5502b951e
Sourav Saha <saha83787@gmail.com>
2024-01-23 20:00:29 +0530

    league standings hook created

--- src/Components/LeagueDetails.jsx
+++ src/Components/LeagueDetails.jsx
@@ -2,33 +2,64 @@ import React, { useContext, useState } from "react";
 import { LeagueIdContext } from "../Contexts/LeagueIdContextProvider";
 import useLeagueFixtures from "../Hooks/useLeagueFixtures";
 import Fixtures from "./Fixtures";
+import LeagueStandingsHeader from "./LeagueStandings/LeagueStandingsHeader";
+import useLeagueStandings from "../Hooks/useLeagueStandings";
+import LeagueStandings from "./LeagueStandings/LeagueStandings";
 
-function LeagueDetails({selectedLeagueId}) {
+function LeagueDetails({ selectedLeagueId }) {
     const { leagueId, setLeagueId } = useContext(LeagueIdContext);
     const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
-    const [leagueMatches,setLeagueMatches]=useState([]);
+    const [leagueMatches, setLeagueMatches] = useState([]);
+    const [standings,setStandings]=useState([]);
     const fixturesData = useLeagueFixtures();
+    const standingsData=useLeagueStandings();
 
     const handleFixtures = (e) => {
         e.preventDefault();
-        setLeagueId({date, selectedLeagueId});
+        setLeagueId({ date, selectedLeagueId });
         console.log(leagueId);
         console.log(fixturesData);
-        setLeagueMatches((prev)=>{
-            const updatedMatches=new Set([prev]);
+        setLeagueMatches((prev) => {
+            const updatedMatches = new Set([prev]);
 
-            fixturesData?.response?.forEach((leagueMatch)=>{
+            fixturesData?.response?.forEach((leagueMatch) => {
                 updatedMatches.add({
-                    time:leagueMatch.fixture.date,
-                    homeTeam:leagueMatch.teams.home.name,
-                    homeTeamLogo:leagueMatch.teams.home.logo,
-                    awayTeam:leagueMatch.teams.away.name,
-                    awayTeamLogo:leagueMatch.teams.away.logo,
-                    win:leagueMatch.goals.home,
-                    lose:leagueMatch.goals.away
+                    time: leagueMatch.fixture.date,
+                    homeTeam: leagueMatch.teams.home.name,
+                    homeTeamLogo: leagueMatch.teams.home.logo,
+                    awayTeam: leagueMatch.teams.away.name,
+                    awayTeamLogo: leagueMatch.teams.away.logo,
+                    win: leagueMatch.goals.home,
+                    lose: leagueMatch.goals.away
                 });
             });
             return [...updatedMatches];
+        });
+    };
+
+    const handleStandingsBtn=(e)=>{
+        e.preventDefault();
+        setLeagueId({date,selectedLeagueId});
+        // console.log(standingsData);
+        setStandings((prev)=>{
+            const updatedStandings=new Set([prev]);
+
+            standingsData?.response[0]?.league?.standings[0]?.forEach((standings)=>{
+                updatedStandings.add({
+                    position:standings.rank,
+                    clubName:standings.team.name,
+                    clubLogo:standings.team.logo,
+                    matchesPlayed:standings.all.played,
+                    won:standings.all.win,
+                    draw:standings.all.draw,
+                    lose:standings.all.lose,
+                    gf:standings.all.goals.for,
+                    ga:standings.all.goals.against,
+                    gd:standings.goalsDiff,
+                    points:standings.points
+                });  
+            });
+            return [...updatedStandings];
         });
     };
 
@@ -55,6 +86,7 @@ function LeagueDetails({selectedLeagueId}) {
                         <button
                             className="font-nb font-semibold text-2xl px-3 py-3 bg-white rounded-md shadow-sm shadow-gray-500 text-blue-600 hover:text-white  hover:bg-blue-600 duration-200 ease-in-out max-md:text-lg max-[425px]:text-sm"
                             id="standingsBtn"
+                            onClick={handleStandingsBtn}
                         >
                             Standings
                         </button>
@@ -82,19 +114,40 @@ function LeagueDetails({selectedLeagueId}) {
                             </button>
                         </div>
                         {
-                            leagueMatches && leagueMatches.map((match,index)=>{
-                                return(
+                            leagueMatches && leagueMatches.map((match, index) => {
+                                return (
                                     <Fixtures key={index}
-                                 time={match.time}
-                                 homeTeam={match.homeTeam}
-                                 homeTeamLogo={match.homeTeamLogo}
-                                 awayTeam={match.awayTeam}
-                                 awayTeamLogo={match.awayTeamLogo}
-                                 win={match.win}
-                                 lose={match.lose}  />
+                                        time={match.time}
+                                        homeTeam={match.homeTeam}
+                                        homeTeamLogo={match.homeTeamLogo}
+                                        awayTeam={match.awayTeam}
+                                        awayTeamLogo={match.awayTeamLogo}
+                                        win={match.win}
+                                        lose={match.lose} />
                                 );
-                        })}    
+                            })}
                     </div>
+                    <LeagueStandingsHeader>
+                        {
+                            standings && standings.map((standing,index)=>{
+                                return(
+                                    <LeagueStandings
+                                    key={index}
+                                    position={standing.position}
+                                    clubLogo={standing.clubLogo}
+                                    clubName={standing.clubName}
+                                    matchesPlayed={standing.matchesPlayed}
+                                    won={standing.won}
+                                    draw={standing.draw}
+                                    lose={standing.lose}
+                                    gf={standing.gf}
+                                    ga={standing.ga}
+                                    gd={standing.gd}
+                                    points={standing.points}
+                                    />
+                                );
+                            })}
+                    </LeagueStandingsHeader>
                 </div>
             </div>
         </div>
